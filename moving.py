cbb096e5011bf6bb65346eee0258f395366c3e99
User10500 <mzawierzeniec@gmail.com>
2019-10-23 14:23:32 +0200

    New pillar added

--- moving.py
+++ moving.py
@@ -16,8 +16,8 @@ white = (255, 255, 255)
 clock = pg.time.Clock()
 catImg = pg.image.load('cat.png')
 
-cat_width = 67
-cat_height = 123
+cat_width = 59
+cat_height = 96
 cat_speed = 0
 gameexit = False
 
@@ -27,31 +27,21 @@ class Movement():
         self.y_change = y_change
         self.event = event
 
-    def move(self):
-        if self.event.type == pg.KEYDOWN:
-            if self.event.key == pg.K_LEFT:
-                self.x_change = -10
-            elif self.event.key == pg.K_RIGHT:
-               self. x_change = 10
-        if self.event.type == pg.KEYUP:
-            if self.event.key == pg.K_LEFT or self.event.key == pg.K_RIGHT:
-                self.x_change = 0
-
     def jump(self):
         if self.event.type == pg.KEYDOWN:
             if self.event.key == pg.K_UP:
                 self.y_change = +10
             elif self.event.key == pg.K_DOWN:
-               self. x_change = -10
+                self.y_change = -10
         if self.event.type == pg.KEYUP:
-            if self.event.key == pg.K_LEFT or self.event.key == pg.K_RIGHT:
+            if self.event.key == pg.K_UP or self.event.key == pg.K_DOWN:
                 self.x_change = 0
 
     def get_y_change(self):
         return self.y_change
 
 
-''''Draws a cat'''
+'''Draws a cat'''
 
 
 class Cat(Movement):
@@ -81,9 +71,9 @@ def text_objects(text, font):
 
 
 def display_message(text):
-    largeText = pg.font.Font('freesansbold.ttf',115)
+    largeText = pg.font.Font('freesansbold.ttf', 115)
     TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = ((display_width/2),(display_height/2))
+    TextRect.center = ((display_width / 2), (display_height / 2))
     gamedisplay.blit(TextSurf, TextRect)
 
     pg.display.update()
@@ -104,11 +94,17 @@ def game_loop():
     y = (display_height * 0.4)
     y_change = 0
 
-    pillar_startx = 600
-    pillar_starty = random.randrange(0, display_height)
     pillar_speed = 7
-    pillar_width = 100
-    pillar_height = 100
+
+    lpillar_startx = 800
+    lpillar_starty = random.randrange(350, display_height)
+    lpillar_width = 150
+    lpillar_height = 600
+
+    upillar_startx = 800
+    upillar_starty = lpillar_starty - 850
+    upillar_width = 150
+    upillar_height = 600
 
     while not gameexit:
 
@@ -121,28 +117,32 @@ def game_loop():
         y -= m.get_y_change()
         gamedisplay.fill(white)
 
-        p = Pillar(pillar_startx, pillar_starty, pillar_width, pillar_height, black)
-        p.place_pillar()
-        pillar_startx -= pillar_speed
+        low = Pillar(lpillar_startx, lpillar_starty, lpillar_width, lpillar_height, black)
+        low.place_pillar()
+        up = Pillar(upillar_startx, upillar_starty, upillar_width, upillar_height, black)
+        up.place_pillar()
+
+        lpillar_startx -= pillar_speed
+        upillar_startx -= pillar_speed
+
         c = Cat(x, y)
         c.display()
 
-        if x > display_width - cat_width or x < 0:
-            crash()
+        if lpillar_startx < 0 - lpillar_width:
+            lpillar_startx = 600 + 2 * lpillar_width
+            lpillar_starty = random.randrange(350, display_height)
+        if upillar_startx < 0 - upillar_width:
+            upillar_startx = 600 + 2 * upillar_width
+            upillar_starty = lpillar_starty - 850
 
-        if pillar_startx < 0 - pillar_width:
-            pillar_startx = 600 + 2*pillar_width
-            pillar_starty = random.randrange(0, display_height)
-
-        if y > pillar_starty + pillar_height: # or y - cat_height > pillar_starty + pillar_height
-            pass
-        elif y + cat_height < pillar_starty:
-            pass
-        else:
-            crash()
+        if y + cat_height > lpillar_starty or y < upillar_starty + upillar_height:
+            if (x + cat_width > lpillar_startx and x < lpillar_startx + lpillar_width or
+                    x + cat_width > upillar_startx and x < upillar_startx + upillar_width):
+                crash()
 
         pg.display.update()
         clock.tick(60)
+
 
 if gameexit == False: game_loop()
 pg.quit()
