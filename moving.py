75ae94cb1a13a60753e6ec503c873a4aaf6c8c15
User10500 <mzawierzeniec@gmail.com>
2019-10-30 19:04:51 +0100

    Smooth jump

--- moving.py
+++ moving.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import time
 import random
+import math
 
 pg.init()
 
@@ -14,30 +15,35 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 
 clock = pg.time.Clock()
-catImg = pg.image.load('cat.png')
+birdImg = pg.image.load('bird.png')
 
-cat_width = 59
-cat_height = 96
+bird_width = 32
+bird_height = 32
 gameexit = False
 
 
 
 
-'''Draws a cat'''
+'''Draws a bird'''
 
 
-class Cat():
+class bird():
 
     startpos = 0
+    jump_width = 350
+    jump_height = 200
+    x_change = -(math.sqrt(-4*(1/jump_width)*(-jump_height)))/(2*(1/jump_width))
+    y_change = 0
     up = True
 
-    def __init__(self, x, y, jump):
+    def __init__(self, x, y, jump, pillar_speed):
         self.x = x
         self.y = y
         self.jump = jump
+        self.pillar_speed = pillar_speed
 
     def display(self):
-        gamedisplay.blit(catImg, (self.x, self.y))
+        gamedisplay.blit(birdImg, (self.x, self.y))
 
     def _jump(self, jump):
         self.jump = jump
@@ -45,15 +51,12 @@ class Cat():
         if not self.jump:
             self.startpos = self.y
             self.jump = True
-        if self.startpos - 100 <= self.y and self.up == True:
-            self.y -= 5
-            print("gora")
-            if self.startpos - 100 == self.y: self.up = False
-        elif self.up == False:
-            print("dol")
-            self.y += 5
-            if self.startpos > self.y:
-                self.jump = False
+            self.x_change = -(math.sqrt(-4*(1/self.jump_width)*(-self.jump_height)))/(2*(1/self.jump_width))
+        print("xchange ", (math.sqrt(-4*(1/self.jump_width)*(-self.jump_height)))/(2*(1/self.jump_width)))
+        self.y = self.startpos + (1 / self.jump_width) * self.x_change * self.x_change - self.jump_height
+        self.x_change += self.pillar_speed
+        print(self.y)
+
 
     def get_y(self):
         return self.y
@@ -97,7 +100,7 @@ def game_loop():
     global m
     global gameexit
     x = (display_width * 0.1)
-    y = (display_height * 0.4)
+    y = (display_height * 0.65)
     jump = False
 
     pillar_speed = 8
@@ -112,7 +115,7 @@ def game_loop():
     upillar_width = 150
     upillar_height = 600
 
-    c = Cat(x, y, jump)
+    c = bird(x, y, jump, pillar_speed)
 
     while not gameexit:
 
@@ -137,19 +140,19 @@ def game_loop():
         lpillar_startx -= pillar_speed
         upillar_startx -= pillar_speed
 
-        #c = Cat(x, y, jump)
+        #c = bird(x, y)
         c.display()
 
         if lpillar_startx < 0 - lpillar_width:
             lpillar_startx = 600 + 2 * lpillar_width
-            lpillar_starty = random.randrange(350, display_height)
+            lpillar_starty = random.randrange(250, display_height - 250)
         if upillar_startx < 0 - upillar_width:
             upillar_startx = 600 + 2 * upillar_width
             upillar_starty = lpillar_starty - 850
 
-        if c.get_y() + cat_height > lpillar_starty or c.get_y() < upillar_starty + upillar_height:
-            if (x + cat_width > lpillar_startx and x < lpillar_startx + lpillar_width or
-                    x + cat_width > upillar_startx and x < upillar_startx + upillar_width):
+        if c.get_y() + bird_height > lpillar_starty or c.get_y() < upillar_starty + upillar_height:
+            if (x + bird_width > lpillar_startx and x < lpillar_startx + lpillar_width or
+                    x + bird_width > upillar_startx and x < upillar_startx + upillar_width):
                 crash()
 
         pg.display.update()
